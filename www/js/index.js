ce524ce2881b1ffabb34b5e6b047bcf1646547d1
Oscar Fonts <oscar.fonts@gmail.com>
2017-03-29 17:54:02 +0200

    Redirect to map

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,7 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+        window.location.replace("mapboxgl.html");
         this.receivedEvent('deviceready');
     },
 
